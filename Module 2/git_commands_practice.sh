b4cac87ac52b584f777bfe6581081b8b24813c9e
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2023-08-29 11:43:59 -0300

    Create git_commands_practice.sh

---
+++ Module 2/git_commands_practice.sh
@@ -0,0 +1,26 @@
+# Initialize a repository
+git init
+
+# Create a file
+git add .
+
+# Commit changes
+git commit
+
+# Create a branch or get a list of branches
+git branch
+
+# Switch to a branch
+git checkout
+
+# Check Status
+git status
+
+# Review commits
+git log
+
+# Revert changes
+git revert
+
+# Merge changes
+git merge
