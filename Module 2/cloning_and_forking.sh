d4270ca0d8006eaa15231b50068f89c8a56f892f
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2023-09-13 17:22:52 -0300

    Update cloning_and_forking.sh

--- Module 2/cloning_and_forking.sh
+++ Module 2/cloning_and_forking.sh
@@ -1,3 +1,15 @@
 # Creates a identical copy of the remote repository
 git clone
 
+# Gets the latest copy of the repo from origin.
+git fetch
+
+# Review the code to compare changes
+git diff
+
+# Update the main branch to origin
+git push
+
+# Both fetch and merge the changes into your local repo
+git pull
+
